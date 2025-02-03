2fc765770336f141c7085587570645dc6ce18fc1
zhn <zzihan0805@gmail.com>
2025-02-03 02:39:14 -0500

    updated home page

--- frontend/web/src/app/home/page.tsx
+++ frontend/web/src/app/home/page.tsx
@@ -6,14 +6,13 @@ import { Sidebar } from "lucide-react";
 
 export default function Home() {
     return (
-        <main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="font-[family-name:var(--font-geist-sans)]">
             <Navbar/>
-            <SidebarProvider>
-                <AppSidebar/>
-            </SidebarProvider>
-            <h1 className="text-4xl font-semibold">Welcome!</h1>
-            <div>
-                <PageCard title="Manage your files" href="/files"/>
+            <div className="flex flex-col min-h-screen justify-center items-center">
+                <h1 className="text-4xl font-semibold">Welcome!</h1>
+                <div>
+                    <PageCard title="Manage your files" href="/files"/>
+                </div>
             </div>
         </main>
     );
--- frontend/web/src/components/navbar.tsx
+++ frontend/web/src/components/navbar.tsx
@@ -2,18 +2,19 @@
 
 import Link from "next/link"
 import Image from "next/image";
+import { Menu } from "lucide-react";
 
 const Navbar = () => {
     return (
-        <nav className="top-0 z-[20] mx-auto w-full flex items-center justify-between bg-background/80 backdrop-blur-sm border-b border-gray-500 p-5">
+        <nav className="z-[20] flex items-center justify-between h-20 mx-auto px-4 bg-background/80 backdrop-blur-sm border-b border-gray-300">
             <h1 className="font-bold text-3xl w-full">
-                oCloud
+                <Link href="/">oCloud</Link>
             </h1>
             <div className="flex">
                 <Link href="/home" className="p-4">Home</Link>
-                <Link href="/about" className="p-4">About</Link>
-                <Link href="/documentation" className="p-4">Documentation</Link>
-                <Link href="/other" className="p-4">Other</Link>
+                <Link href="/#about" className="p-4">About</Link>
+                <Link href="https://github.com/niooii/ocloud" className="p-4">Documentation</Link>
+                <Link href="/#other" className="p-4">Other</Link>
             </div>
         </nav>
     );
