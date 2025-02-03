e60c780ec78bc92e6d74d7d66610657be8439a3d
zhn <zzihan0805@gmail.com>
2025-02-03 00:38:03 -0500

    start reworking components

--- frontend/web/src/app/layout.tsx
+++ frontend/web/src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar/>
         <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
           {children}
           <Toaster />
--- frontend/web/src/components/navbar.tsx
+++ frontend/web/src/components/navbar.tsx
@@ -2,66 +2,21 @@
 
 import Link from "next/link"
 import Image from "next/image";
-import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuIndicator,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    NavigationMenuViewport,
-    navigationMenuTriggerStyle
-  } from "@/components/ui/navigation-menu"
-import { Card } from "./ui/card";
 
-export default function Navbar() {
+const Navbar = () => {
     return (
-        <div className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-sm z-50">
-            <div className="container flex h-14 items-center">
-                <nav className="flex items-center space-x-6 text-sm font-medium p-4">
-                    <Image
-                        src="/onion.svg"
-                        alt="onion"
-                        width={50}
-                        height={50}
-                        priority
-                        className="brightness-0 invert"
-                    />
-                    <NavigationMenu>
-                        <NavigationMenuList>
-                            <NavigationMenuItem>
-                                <NavigationMenuTrigger>Menu</NavigationMenuTrigger>
-
-                                <NavigationMenuContent>
-                                    <NavigationMenuLink href="/home">
-                                        <Card className="p-4">
-                                            Home
-                                        </Card>
-                                    </NavigationMenuLink>
-
-                                    <NavigationMenuLink href="/account">
-                                        <Card className="p-4">
-                                            Account
-                                        </Card>
-                                    </NavigationMenuLink>
-                                    
-                                    <NavigationMenuLink href="/setting">
-                                        <Card className="p-4">
-                                            Setting
-                                        </Card>
-                                    </NavigationMenuLink>
-                                </NavigationMenuContent>
-                                
-                                <NavigationMenuLink href="https://github.com/niooii/ocloud" className={navigationMenuTriggerStyle()}>
-                                    Documentation
-                                </NavigationMenuLink>
-                            
-                            </NavigationMenuItem>
-                        </NavigationMenuList>
-                    </NavigationMenu>
-                </nav>
+        <header className = "top-0 z-[20] mx-auto flex w-full items-center justify-between bg-background/80 backdrop-blur-sm border-b border-gray-500 p-5">
+            <div>
+                1
             </div>
-        </div>
-    )
-}
+            <div>
+                2
+            </div>
+            <div>
+                3
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
